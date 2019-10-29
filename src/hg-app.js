4742460413c6b71ee37b897d7f291a413317c04a
Michał Kownacki <michal_kownacki@o2.pl>
2019-10-29 15:01:45 +0100

    Use firebase with es modules.

--- src/hg-app.js
+++ src/hg-app.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'https://unpkg.com/lit-element@^2.2.1/lit-element.js?module';
+import firebase from 'firebase/app';
 import 'https://unpkg.com/@material/mwc-button@^0.9.1/mwc-button.js?module';
 import 'https://unpkg.com/@polymer/app-route@^3.0.2/app-location.js?module';
 import 'https://unpkg.com/@polymer/app-route@^3.0.2/app-route.js?module';
@@ -6,6 +7,17 @@ import './hg-header.js';
 import './hg-landing.js';
 import './hg-menu/hg-menu.js';
 import './hg-gallery/hg-gallery.js';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDvamIugzBC3k3WA52KpHeINrfDHfkvnSs",
+  authDomain: "pl-hotelgarden.firebaseapp.com",
+  databaseURL: "https://pl-hotelgarden.firebaseio.com",
+  projectId: "pl-hotelgarden",
+  storageBucket: "pl-hotelgarden.appspot.com",
+  messagingSenderId: "439170507609",
+  appId: "1:439170507609:web:d50495f3bf9c9613702248",
+  measurementId: "G-T7DQCNYLP2"
+});
 
 customElements.define('hg-app', class extends LitElement {
   static get properties() {
--- src/hg-gallery/hg-gallery-upload.js
+++ src/hg-gallery/hg-gallery-upload.js
@@ -1,4 +1,6 @@
 import {LitElement, html, css} from 'https://unpkg.com/lit-element@^2.2.1/lit-element.js?module';
+import firebase from 'firebase/app';
+import 'firebase/storage';
 import 'https://unpkg.com/@polymer/paper-icon-button@^3.0.2/paper-icon-button.js?module';
 import 'https://unpkg.com/@polymer/iron-icons@^3.0.1/iron-icons.js?module';
 
