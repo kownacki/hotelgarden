6ff248ca8ba5bbf0716c63899a433fd0cbb741d8
Michał Kownacki <michal_kownacki@o2.pl>
2019-11-30 16:42:08 +0100

    Add more to links.

--- src/utils.js
+++ src/utils.js
@@ -52,7 +52,7 @@ export const links = [
     path: '/',
     sublinks: [
       {name: 'O hotelu', path: '/', image: 'https://picsum.photos/id/1040/500/500'},
-      {name: 'Villa Garden', path: '/villa-garden', image: 'https://picsum.photos/id/84/500/500'},
+      {name: 'Villa Garden', path: '/villa-garden', image: '/resources/images/villa.jpg'},
       {name: 'Oleśnica i okolice', path: '/olesnica-i-okolice', image: 'https://picsum.photos/id/608/500/500'},
       {name: 'Opinie ', path: '/opnie', image: 'https://picsum.photos/id/442/500/500'},
       {name: 'FAQ ', path: '/faq', image: 'https://picsum.photos/id/471/500/500'},
@@ -64,19 +64,28 @@ export const links = [
     name: 'Konferencje',
     path: '/konferencje',
     sublinks: [
-      {name: 'Nasze sale', path: '/konferencje'},
-      {name: 'Wigilie Firmowe', path: '/konferencje'},
+      {name: 'Nasze sale', path: '/nasze-sale', image: 'https://picsum.photos/id/155/500/500'},
+      {name: 'Wigilie Firmowe', path: '/wigilie-firmowe', image: 'https://picsum.photos/id/255/500/500'},
     ],
   }, {
     name: 'Kuchnia',
     path: '/kuchnia',
     sublinks: [
-      {name: 'O naszej kuchni', path: '/kuchnia'},
-      {name: 'Restauracja Magnolia', path: '/kuchnia'},
-      {name: 'Grill Garden', path: '/kuchnia'},
-      {name: 'Catering', path: '/kuchnia'},
+      {name: 'O naszej kuchni', path: '/kuchnia', image: 'https://picsum.photos/id/75/500/500'},
+      {name: 'Restauracja Magnolia', path: '/restauracja-magnolia', image: 'https://picsum.photos/id/175/500/500'},
+      {name: 'Grill Garden', path: '/grill-garden', image: 'https://picsum.photos/id/275/500/500'},
+      {name: 'Catering', path: '/catering', image: 'https://picsum.photos/id/375/500/500'},
     ],
   }, {
+    name: 'Uroczystości',
+    path: '/uroczystości',
+    sublinks: [
+      {name: 'Wesela', path: '/wesela', image: 'https://picsum.photos/id/256/500/500'},
+      {name: 'Chrzciny', path: '/chrzciny', image: 'https://picsum.photos/id/257/500/500'},
+      {name: 'Komunie', path: '/komunie', image: 'https://picsum.photos/id/258/500/500'},
+    ],
+  },
+  {
     name: 'Galeria',
     path: '/galeria',
   }, {
