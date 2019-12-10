45cf280d9f22176be97a2ad5a58f138c5b0a7714
Michał Kownacki <michal_kownacki@o2.pl>
2019-12-10 12:57:40 +0100

    Add h3 and h4 option to hg-heading.

--- src/hg-heading.js
+++ src/hg-heading.js
@@ -4,6 +4,8 @@ customElements.define('hg-heading', class extends LitElement {
   static get properties() {
     return {
       center: {type: Boolean, reflect: true},
+      h3: {type: Boolean},
+      h4: {type: Boolean},
     };
   }
   static get styles() {
@@ -17,16 +19,34 @@ customElements.define('hg-heading', class extends LitElement {
         text-align: center;
         margin: 60px 0;
       }
-      h2 {
+      :host([h3]) {
+        margin-bottom: 27px;
+      }
+      :host([h4]) {
+        margin-bottom: 24px;
+      }
+      h2, h3, h4 {
         margin: 0;
         font-weight: 300;
+      }
+      h2 {
         font-size: 40px;
+      }
+      h3 {
+        font-size: 35px;
+      }
+      h4 {
+        font-size: 30px;
       }
     `;
   }
   render() {
     return html`
-      <h2 id="editable"><slot></slot></h2>
+      ${this.h4
+        ? html`<h4 id="editable"><slot></slot></h4>`
+        : this.h3
+        ? html`<h3 id="editable"><slot></slot></h3>`
+        : html`<h2 id="editable"><slot></slot></h2>`}
     `;
   }
 });
