786a70ceb08b6865404c8c03e4beb802113daf73
Michał Kownacki <michal_kownacki@o2.pl>
2019-12-31 17:04:31 +0100

    Add more to rooms page.

--- src/pages/rooms/hg-rooms.js
+++ src/pages/rooms/hg-rooms.js
@@ -18,17 +18,22 @@ customElements.define('hg-rooms', class extends LitElement {
     return html`
       <hg-article .uid=${'rooms'}></hg-article>
       <hg-icons .uid=${'rooms'}></hg-icons>
-      <hg-heading center>${'Wyposażenie każdego pokoju'}</hg-heading>
+      <hg-heading h3 center>${'Wyposażenie każdego pokoju'}</hg-heading>
       <hg-icons .uid=${'rooms-facilities'}></hg-icons>
       ${_.map((index) => html`
-        <hg-text-image 
+        <hg-text-image
           .uid=${'rooms-' + index}
           .iconFields=${['size', 'people']}
           .iconSrcs=${[
             'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fediting%2Fmove.png?alt=media&token=68fa9540-cd2c-4577-9a32-55c83d5ea682',
             'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fpeople%2Fstanding-man.png?alt=media&token=f284442d-c273-480e-acd1-67a9bcb0463a'
-          ]}>
+          ]}
+          .buttons=${[{url: '#', text: 'Rezerwuj pokój'}]}>
         </hg-text-image>
+        ${index === 1 || index === 4 ? '' : html`
+          <hg-heading h3 center>${'Dodatkowe udogodnienia pokoju'}</hg-heading>
+          <hg-icons .uid=${'rooms-'+ index}></hg-icons>
+        `}
         <hg-content-slider .uid=${'rooms-'+ index}></hg-content-slider>
       `, [1, 2, 3, 4])}
       <hg-heading center>${'Warunki rezerwacji'}</hg-heading>
