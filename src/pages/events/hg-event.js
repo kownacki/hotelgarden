51780d6df2aff1f512f6ee9eae804fbc40d2e02c
Michał Kownacki <michal_kownacki@o2.pl>
2020-01-03 21:28:04 +0100

    Add changing date to events.

--- src/pages/events/hg-event.js
+++ src/pages/events/hg-event.js
@@ -1,8 +1,9 @@
 import {LitElement, html, css} from 'lit-element';
 import moment from 'moment';
-import {db} from "../../utils.js";
+import {db, updateData} from "../../utils.js";
 import '../../elements/hg-banner.js';
 import './hg-events/hg-events-sidebar.js';
+import './hg-event/hg-event-edit-date.js';
 
 customElements.define('hg-event', class extends LitElement {
   static get properties() {
@@ -26,10 +27,19 @@ customElements.define('hg-event', class extends LitElement {
       .content {
         flex: 1;
       }
+      .header {
+        display: flex;
+      }
       .date {
         font-size: 20px;
         color: var(--secondary-color);
         margin-bottom: 10px;
+      }
+      hg-event-edit-date {
+        display: none;
+      }
+      .header:hover hg-event-edit-date, hg-event-edit-date[opened] {
+        display: block;
       }
       .divider {
         height: 1px;
@@ -43,23 +53,37 @@ customElements.define('hg-event', class extends LitElement {
   }
   async updated(changedProperties) {
     if (changedProperties.has('uid')) {
-      db.collection('events').doc(this.uid).get()
+      db.doc('events/' + this.uid).get()
         .then((snapshot) => this._event = {...snapshot.data(), address: snapshot.id});
-      this._content = (await db.collection('eventsContents').doc(this.uid).get()).get('content');
+      this._content = (await db.doc('eventsContents/' + this.uid).get()).get('content');
     }
+  }
+  async updateData(path, data) {
+    return updateData('events/' + this.uid, path, data);
   }
   render() {
     return this._event ? html`
       <hg-banner .doc=${`events/${this.uid}`} .noSubheading=${true} .useTitleAsHeading=${true}></hg-banner>
       <div class="container">
-        <div class="content"> 
-          <div class="date">
-            ${moment().isBefore(this._event.date, 'day') 
-              ? 'Odbędzie się'
-              : moment().isSame(this._event.date, 'day') 
-              ? 'Dzisiaj'
-              : 'Odbyło się'}
-            ${moment(this._event.date).format('D MMMM YYYY')}</div>
+        <div class="content">
+          <div class="header">
+            <div class="date">
+              ${moment().isBefore(this._event.date, 'day')
+                ? 'Odbędzie się'
+                : moment().isSame(this._event.date, 'day')
+                  ? 'Dzisiaj'
+                  : 'Odbyło się'}
+              ${moment(this._event.date).format('D MMMM YYYY')}
+            </div>
+            <hg-event-edit-date 
+              .date=${this._event.date}
+              @save=${(event) => {
+                this._event.date = event.detail;
+                this.requestUpdate();
+                this.updateData('date', event.detail);
+              }}>
+            </hg-event-edit-date>
+          </div>
           <div class="divider"></div>
           ${this._content}
         </div>
---
+++ src/pages/events/hg-event/hg-event-edit-date.js
@@ -0,0 +1,54 @@
+import {LitElement, html, css} from 'lit-element';
+import moment from 'moment';
+
+customElements.define('hg-event-edit-date', class extends LitElement {
+  static get properties() {
+    return {
+      date: String,
+      opened: {type: Boolean, reflect: true},
+    };
+  }
+  static get styles() {
+    return css`
+      :host {
+        margin-left: 8px;
+      }
+      paper-icon-button {
+        width: 24px;
+        height: 24px;
+        padding: 0;
+      }
+      .buttons {
+        margin-top: 10px;
+      }
+    `;
+  }
+  render() {
+    return html`
+      <paper-icon-button 
+        .icon=${'image:edit'}
+         @click=${() => this.shadowRoot.getElementById('dialog').open()}>
+      </paper-icon-button>
+      <paper-dialog 
+        id="dialog"
+        @opened-changed=${(event) => {this.opened = event.target.opened; this.dispatchEvent(new CustomEvent('opened-changed'))}}>
+        <div>
+          Zmień datę wydarzenia
+        </div>
+        <input 
+          type="date"
+          name="date" 
+          id="date" 
+          min="${moment().format('YYYY-MM-DD')}"
+          .value=${this.date}>
+        <div class="buttons">
+          <paper-button raised @click=${() => this.shadowRoot.getElementById('dialog').close()}>Anuluj</paper-button>   
+          <paper-button raised @click=${() => {
+            this.shadowRoot.getElementById('dialog').close();
+            this.dispatchEvent(new CustomEvent('save', {detail: this.shadowRoot.getElementById('date').value, composed: true}));
+          }}>Zapisz</paper-button>
+        </div>
+      </paper-dialog>
+    `;
+  }
+});
