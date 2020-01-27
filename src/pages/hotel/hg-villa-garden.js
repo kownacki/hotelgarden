b28d66948bffe24c267379cbc00d54e40e707d5e
Michał Kownacki <michal_kownacki@o2.pl>
2020-01-27 02:59:53 +0100

    Add Villa room to Villa Garden page.

--- src/pages/hotel/hg-villa-garden.js
+++ src/pages/hotel/hg-villa-garden.js
@@ -1,8 +1,10 @@
 import {LitElement, html, css} from 'lit-element';
-import {staticProp, openProfitroom} from '../../utils.js';
+import {staticProp} from '../../utils.js';
 import '../../content/hg-article/hg-intro-article.js';
-import '../../content/hg-mosaic.js';
 import '../../content/hg-links.js';
+import '../rooms/hg-room.js';
+
+const VILLA_ROOM_INDEX = 3;
 
 customElements.define('hg-villa-garden', class extends LitElement {
   static get properties() {
@@ -18,10 +20,10 @@ customElements.define('hg-villa-garden', class extends LitElement {
   render() {
     return html`
       <hg-intro-article .uid=${'villa-garden'}></hg-intro-article>
-      <hg-text-image .uid=${'villa-garden'} .buttons=${[
-        {click: () => openProfitroom('villa'), text: 'Rezerwuj'},
-        {url: '/pokoje', text: 'Zobacz wszystkie pokoje'},
-      ]}></hg-text-image>
+      <hg-room 
+        .index=${VILLA_ROOM_INDEX} 
+        .extraButtons=${staticProp([{url: '/pokoje', text: 'Zobacz pozostałe pokoje'}])}>
+      </hg-room>
       <hg-links .path=${'/villa-garden'} .superpath=${'/'}></hg-links>
     `;
   }
--- src/pages/rooms/hg-rooms.js
+++ src/pages/rooms/hg-rooms.js
@@ -1,11 +1,9 @@
 import {LitElement, html, css} from 'lit-element';
-import {staticProp, ROOM_ID, openProfitroom} from '../../utils.js';
 import sharedStyles from '../../styles/shared-styles.js'
 import '../../content/hg-article/hg-intro-article.js';
 import '../../content/hg-content-icons.js';
-import '../../elements/hg-icons.js';
 import '../../elements/hg-heading.js';
-// import './hg-room.js';
+import './hg-room.js';
 
 customElements.define('hg-rooms', class extends LitElement {
   static get properties() {
@@ -13,46 +11,15 @@ customElements.define('hg-rooms', class extends LitElement {
   }
   static get styles() {
     return [sharedStyles, css`
-      .room hg-text-image {
-        margin-bottom: 40px;
-      }
-      .room hg-content-icons {
-        margin-top: 40px;
-      }
     `];
   }
   render() {
     return html`
       <hg-intro-article .uid=${'rooms'}></hg-intro-article>
       <hg-content-icons .uid=${'rooms'}></hg-content-icons>
-      ${_.map.convert({cap: false})((rooms, index) => html`
-        <div class="room divider">
-          <hg-text-image
-            .uid=${'rooms-' + (index + 1)}
-            .iconFields=${staticProp(['size', 'people'])}
-            .iconSrcs=${staticProp([
-              'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fediting%2Fmove.png?alt=media&token=68fa9540-cd2c-4577-9a32-55c83d5ea682',
-              'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fpeople%2Fstanding-man.png?alt=media&token=f284442d-c273-480e-acd1-67a9bcb0463a'
-            ])}
-            .icons=${true}
-            .buttons=${_.map(
-              (room) => ({click: () => openProfitroom(room.location, room.id, room.adults), text: room.text || 'Rezerwuj pokój'}),
-            rooms)}>
-          </hg-text-image>
-          <hg-content-icons .small=${true} .uid=${'rooms-' + (index + 1)}></hg-content-icons>
-        </div>
-      `, [
-        [
-          {location: 'hotelgarden', id: ROOM_ID.doubleClassic, text: 'Rezerwuj pokój (łóżka razem)'}, 
-          {location: 'hotelgarden', id: ROOM_ID.twin, text: 'Rezerwuj pokój (łóżka osobno)'}
-        ],
-        [{location: 'hotelgarden', id: ROOM_ID.superior}],
-        [{location: 'villagarden', id: ROOM_ID.villaRoom}],
-        [
-          {location: 'hotelgarden', id: ROOM_ID.tripleClassic, adults: 3, text: 'Rezerwuj pokój (3 osoby)'},
-          {location: 'hotelgarden', id: ROOM_ID.quadClassic, adults: 4, text: 'Rezerwuj pokój (4 osoby)'}
-        ],
-      ])}
+      ${_.map((index) => html`
+        <hg-room .index=${index}></hg-room>
+      `, [1, 2, 3, 4])}
       <hg-heading center>${'Warunki rezerwacji'}</hg-heading>
       <hg-content-icons .uid=${'rooms-conditions'}></hg-content-icons>
     `;
