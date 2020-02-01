df8ee69402c532c2cc196a8cac4eff9272595612
Michał Kownacki <michal_kownacki@o2.pl>
2020-02-01 21:10:21 +0100

    Add google index verification file.

--- rollup.config.js
+++ rollup.config.js
@@ -21,6 +21,7 @@ export default [{
         {src: 'src/styles.css', dest: 'dist/src'},
         {src: 'resources', dest: 'dist'},
         {src: 'node_modules/@ckeditor', dest: 'dist/node_modules'},
+        {src: 'googlec2ac75b505b73639.html', dest: 'dist'},
       ],
     }),
     terser(),
