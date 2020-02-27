f398ca7cd908b8ee84e088f8ebe86652dd36dcba
Michał Kownacki <michal_kownacki@o2.pl>
2020-02-27 23:36:10 +0100

    Send analytics only when on deployed website.

--- src/hg-app.js
+++ src/hg-app.js
@@ -78,8 +78,10 @@ customElements.define('hg-app', class extends LitElement {
   }
   async updated(changedProperties) {
     if (changedProperties.has('_path')) {
-      ga('set', 'page', this._path);
-      ga('send', 'pageview');
+      if (window.location.hostname === 'www.hotelgarden.pl') {
+        ga('set', 'page', this._path);
+        ga('send', 'pageview');
+      }
       if (/^\/wydarzenia\/[^\/]+$/.test(this._path)) {
         this._event = true;
         this._uid =_.replace('/wydarzenia/', '', this._path);
