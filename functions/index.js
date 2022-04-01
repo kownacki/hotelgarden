1430022294f4f7833099df7c4876e1a7e253618d
Michał Kownacki <mk@michalkownacki.pl>
2022-04-01 19:30:53 +0200

    Use mk-firebase-functions-utils and mk-firebase-utils in functions.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,4 @@
-import admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-
-admin.initializeApp(functions.config().firebase);
 
 import {sendMessage as sendMessageFn} from './sendMessage.js';
 export const sendMessage = functions.region('europe-west1').https.onRequest(sendMessageFn);
--- functions/sendMessage.js
+++ functions/sendMessage.js
@@ -1,61 +1,39 @@
 import _ from 'lodash/fp.js';
-import admin from 'firebase-admin';
+import {sendMessage as mkSendMessage} from 'mk-firebase-functions-utils/sendMessage.js';
 import moment from 'moment-timezone';
+import {firebaseUtils as fb} from './firebase.js';
+
 moment.locale('pl');
-import nodemailer from 'nodemailer';
-
-import corsAsyncLib from './cors-async.js';
-const corsAsync = corsAsyncLib({origin: true});
-
-const generateUid = (timestamp = Date.now()) => `${timestamp}${_.padCharsStart('0', 9,  _.random(1, 10**9 - 1))}`;
 
 // Rough limit, to prevent attacks
 const MAX_MESSAGE_SIZE = 10000;
 
-const sendMail = async (body) => {
-  const config = (await admin.firestore().doc('_config/admin').get()).data().sendMessage;
-  const mailOptions = {
-    to: config.mailOptions.to[body.subject],
-    replyTo: body.email,
-    subject: `${config.mailOptions.subject} | ${body.email}`,
-    html: `
-      <p>Dotyczy: ${body.subject}</p>
-      <p>Imię i nazwisko: ${body.name}</p>
-      <p>Firma: ${body.company || 'Nie podano'}</p>
-      <p>Telefon: ${body.phone}</p>
-      <p>Email: ${body.email}</p>
-      <p>Wiadomość: ${_.replace(/\n/g, '<br>')(body.text)}</p>
-    `,
-  };
-  const mailTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: config.mailTransport.user,
-      pass: config.mailTransport.pass,
-    },
-  });
-  await mailTransport.sendMail(mailOptions);
-  return mailOptions.to;
-};
-
 export const sendMessage = async (req, res) => {
   const now = Date.now();
-  await corsAsync(req, res);
-  if (JSON.stringify(req.body).length > MAX_MESSAGE_SIZE) {
-    return res.status(400).send('Message is too big.');
-  }
-  let to;
-  try {
-    to = await sendMail(req.body);
-    res.status(200).end();
-  } catch (error) {
-    res.status(500).send('Error while sending contact form message.');
-    throw error;
-  }
-  await admin.firestore().doc('sentMessages/' + generateUid(now)).set({
-    ...req.body,
-    to,
+  const config = await fb.get(fb.path('_config/admin', 'sendMessage'));
+  const options = {
+    mailOptions: {
+      to: config.mailOptions.to[req.body.subject],
+      replyTo: req.body.email,
+      subject: `${config.mailOptions.subject} | ${req.body.email}`,
+      getHtml: (body) => `
+        <p>Dotyczy: ${body.subject}</p>
+        <p>Imię i nazwisko: ${body.name}</p>
+        <p>Firma: ${body.company || 'Nie podano'}</p>
+        <p>Telefon: ${body.phone}</p>
+        <p>Email: ${body.email}</p>
+        <p>Wiadomość: ${_.replace(/\n/g, '<br>')(body.text)}</p>
+      `,
+    },
+    mailTransport: config.mailTransport,
     timestamp: now,
-    time: moment(now).tz('Poland').format('LLLL'),
+    maxMessageSize: MAX_MESSAGE_SIZE,
+  };
+  await mkSendMessage(req, res, options);
+  await fb.update(fb.path(`sentMessages/${fb.generateUid(now)}`), {
+    ..._.mapValues(_.replace(/\n/g, '\\n'), req.body),
+    to: options.mailOptions.to,
+    timestamp: options.timestamp,
+    time: moment(options.timestamp).tz('Poland').format('LLLL'),
   });
 };
