df6c7e8e9b8dd5e38e0c615327c0842bdde11b2a
Michał Kownacki <mk@michalkownacki.pl>
2022-04-07 02:48:04 +0200

    Add minInstances: 1 to render function.

--- functions/index.js
+++ functions/index.js
@@ -4,4 +4,4 @@ import {sendMessage as sendMessageFn} from './sendMessage.js';
 export const sendMessage = functions.region('europe-west1').https.onRequest(sendMessageFn);
 
 import {render as renderFn} from './render.js';
-export const render = functions.https.onRequest(renderFn);
+export const render = functions.runWith({minInstances: 1}).https.onRequest(renderFn);
