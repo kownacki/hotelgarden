b4f77502d693c241888a5d090bba6f7c449026d6
Michał Kownacki <mk@michalkownacki.pl>
2022-04-07 02:52:27 +0200

    Synchronize seo config instead of downloading every time render runs.

--- functions/render.js
+++ functions/render.js
@@ -1,11 +1,20 @@
 import _ from 'lodash/fp.js';
+import {getFirestore} from 'firebase-admin/firestore';
 import {createIndex} from './createIndex.js';
-import {firebaseUtils as fb} from './firebase.js';
+
+let seo;
+let resolveSeoReady;
+const seoReady = new Promise((resolve) => resolveSeoReady = resolve);
+
+getFirestore().doc('_config/client').onSnapshot((doc) => {
+  seo = doc.data().seo;
+  resolveSeoReady();
+});
 
 const createDocumentTitle = (title, seo) => `${title} ${seo.titleSeparator} ${seo.titleSuffix}`;
 
 export const render = async (req, res) => {
-  const seo = await fb.get(fb.path('_config/client', 'seo'));
+  await seoReady;
 
   const pageTitle = _.get(`${req.path}.title`, seo.urls);
   const fullTitle = createDocumentTitle(pageTitle, seo);
