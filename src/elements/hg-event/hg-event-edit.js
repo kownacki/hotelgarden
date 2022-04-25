6274c6020140a67a8eb1f7690d98d92581205f36
Michał Kownacki <mk@michalkownacki.pl>
2022-04-26 01:40:25 +0200

    Add tooltips and snackbars to event promoting and publishing.

--- src/elements/hg-event/hg-event-edit.js
+++ src/elements/hg-event/hg-event-edit.js
@@ -1,4 +1,5 @@
 import {LitElement, html, css} from 'lit';
+import '@material/mwc-snackbar';
 import '@material/mwc-switch';
 import {isEventUpcoming, isEventPast} from '../../../utils/events.js';
 import sharedStyles from '../../styles/shared-styles.js';
@@ -25,12 +26,30 @@ export class HgEventEdit extends LitElement {
     hg-event-public-switch, hg-event-promote-switch {
       margin-left: 14px;
     }
+    mwc-snackbar {
+      z-index: 104;
+    }
     @media all and (max-width: 599px) {
       hg-event-edit-date {
         margin-left: 0;
       }
     }
   `];
+  _getPublicSwitchTooltip() {
+    return 'Publiczne wydarzenia są widoczne na stronie wydarzeń oraz będą indeksowane przez przeglądarkę. ' +
+      'Niepubliczne wydarzenia wciąż są dostępne dla użytkowników, ale wyłączne poprzez bezpośredni URL.';
+  }
+  _getPromoteSwitchTooltip(event) {
+    return isEventPast(event)
+      ? 'Nie można promować minionego wydarzenia.'
+      : 'Promowane wydarzenie będzie widoczne w głownym menu nawigacji. ' +
+        'Promowanie zostanie automatycznie wyłączone gdy wydarzenie przestanie być aktualne. ' +
+        'Tylko jedno wydarzenie może być promowane na raz.';
+  }
+  _closeAllSnackbars() {
+    const snackbars = this.shadowRoot.querySelectorAll('mwc-snackbar');
+    snackbars.forEach((snackbar) => snackbar.close());
+  }
   render() {
     return html`
       <div class="container cms">
@@ -40,21 +59,55 @@ export class HgEventEdit extends LitElement {
             this.dispatchEvent(new CustomEvent('request-change', {detail: {field: HgEventEditFields.DATE, value: date}}));
           }}>
         </hg-event-edit-date>
-        <hg-event-public-switch
-          .selected=${this.eventData.event.public}
-          @public-changed=${({detail: publicValue}) => {
-            this.dispatchEvent(new CustomEvent('request-change', {detail: {field: HgEventEditFields.PUBLIC, value: publicValue}}));
-          }}>
-        </hg-event-public-switch>
-        <div title=${isEventPast(this.eventData.event) ? 'Nie można promować minionego wydarzenia' : ''}>
+        <div title=${this._getPublicSwitchTooltip()}>
+          <hg-event-public-switch
+            .selected=${this.eventData.event.public}
+            @public-changed=${({detail: publicValue}) => {
+              this.dispatchEvent(new CustomEvent('request-change', {detail: {field: HgEventEditFields.PUBLIC, value: publicValue}}));
+              this._closeAllSnackbars();
+              if (publicValue) {
+                this.shadowRoot.getElementById('snackbar-public-true').show();
+              } else {
+                this.shadowRoot.getElementById('snackbar-public-false').show();
+              }
+            }}>
+          </hg-event-public-switch>
+        </div>
+        <div title=${this._getPromoteSwitchTooltip(this.eventData.event)}>
           <hg-event-promote-switch
             .selected=${isEventUpcoming(this.eventData.event) && this.promotedEventData?.uid === this.eventData.uid}
             .disabled=${isEventPast(this.eventData.event)}
             @promoted-changed=${({detail: promoted}) => {
               this.dispatchEvent(new CustomEvent('request-change', {detail: {field: HgEventEditFields.PROMOTED, value: promoted}}));
+              this._closeAllSnackbars();
+              if (promoted) {
+                this.shadowRoot.getElementById('snackbar-promote-true').show();
+              } else {
+                this.shadowRoot.getElementById('snackbar-promote-false').show();
+              }
             }}>
           </hg-event-promote-switch>
         </div>
+        <mwc-snackbar
+          id="snackbar-public-true"
+          .leading=${true}
+          .labelText=${'Upubliczniono wydarzenie.'}>
+        </mwc-snackbar>
+        <mwc-snackbar
+          id="snackbar-public-false"
+          .leading=${true}
+          .labelText=${'Wydarzenie przestało być publiczne.'}>
+        </mwc-snackbar>
+        <mwc-snackbar
+          id="snackbar-promote-true"
+          .leading=${true}
+          .labelText=${'Zapisano. Promowane wydarzenie będzie widoczne w głownym menu nawigacji po odświeżeniu strony.'}>
+        </mwc-snackbar>
+        <mwc-snackbar
+          id="snackbar-promote-false"
+          .leading=${true}
+          .labelText=${'Zapisano. Wydarzenie przestało być promowane. Zmiany będą widoczne po odświeżeniu strony.'}>
+        </mwc-snackbar>
       </div>
     `;
   }
