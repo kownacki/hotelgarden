b0e30468e123ada3e90e1f160130f762b5c6979a
Michał Kownacki <mk@michalkownacki.pl>
2022-05-05 05:40:56 +0200

    Add min and max to date-picker.

--- src/edit/hg-date-picker.js
+++ src/edit/hg-date-picker.js
@@ -1,9 +1,12 @@
 import {LitElement, html, css} from 'lit';
+import {ifDefined} from 'lit/directives/if-defined.js';
 import sharedStyles from '../styles/shared-styles.js';
 
 export class HgDatePicker extends LitElement {
   static properties = {
     date: String,
+    min: String,
+    max: String,
     // observables
     picker: Element,
   };
@@ -21,7 +24,8 @@ export class HgDatePicker extends LitElement {
         id="picker"
         type="date"
         name="date" 
-        min="${moment().format('YYYY-MM-DD')}"
+        min=${ifDefined(this.min)}
+        max=${ifDefined(this.max)}
         .value=${this.date}
         @input=${() => {
           this.dispatchEvent(new CustomEvent('date-changed', {detail: this.picker.value}));
