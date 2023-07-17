bc4bb673cbde9e61f9cda829cbc4b9247f955c37
Michał Kownacki <mk@michalkownacki.pl>
2023-07-17 14:53:01 +0200

    Change 'people' icon in hg-hall. Allow hiding setOuts.

--- src/content/hg-hall.js
+++ src/content/hg-hall.js
@@ -1,4 +1,5 @@
 import {LitElement, html, css} from 'lit';
+import {when} from 'lit/directives/when.js';
 import {staticProp} from '../utils.js';
 import sharedStyles from '../styles/shared-styles.js'
 import './hg-text-image.js';
@@ -7,6 +8,7 @@ import './hg-hall/hg-hall-tables.js';
 export class HgHall extends LitElement {
   static properties = {
     uid: String,
+    areSetOutsHidden: Boolean,
   };
   static styles = sharedStyles;
   render() {
@@ -18,10 +20,13 @@ export class HgHall extends LitElement {
           .iconFields=${staticProp(['size', 'people'])}
           .iconSrcs=${staticProp([
             'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fediting%2Fmove.png?alt=media&token=68fa9540-cd2c-4577-9a32-55c83d5ea682',
-            'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fpeople%2Fstanding-man.png?alt=media&token=f284442d-c273-480e-acd1-67a9bcb0463a'
+            'https://firebasestorage.googleapis.com/v0/b/pl-hotelgarden.appspot.com/o/icons%2Fpeople%2Fcrowd.png?alt=media&token=b6252c3c-5c24-4afe-b24e-ee3159645627',
           ])}>
         </hg-text-image>
-        <hg-hall-tables .uid=${this.uid}></hg-hall-tables>
+        ${when(
+          !this.areSetOutsHidden,
+          () => html`<hg-hall-tables .uid=${this.uid}></hg-hall-tables>`
+        )}
       </div>
     `;
   }
